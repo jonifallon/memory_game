a6651d061edd2925b84cfe3b3719bce1276bbe15
Joni Fallon <joni.fallon@libertymutual.com>
2017-03-07 12:00:05 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,39 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//array of cards
+var cards = ["queen", "queen", "king", "king"];
+
+//empty array to hold cards that are in play
+var cardsInPlay = [];
+
+
+//variable to represent first card flipped
+var cardOne = cards[0];
+
+//variable to represent 2nd card flipped
+var cardTwo = cards[2];
+
+//add first card to the cardsInPlay array
+cardsInPlay.push(cardOne);
+
+//add 2nd card to cardsInPlay array
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+//display card that user flipped
+console.log("User flipped " + cardTwo);
+
+//add logic to check to see if two cards have been played
+//if not, don't display any alert
+//if yes, alert if they have a match or not
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	}
+	  else {
+	  	alert("Sorry, try again.")
+	  }
+}  else {
+	
+}
